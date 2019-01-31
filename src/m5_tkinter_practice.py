1e0b3be3d32192d29e9f5a7da275f7b2f2be38bc
smithhm <smithhm@rose-hulman.edu>
2019-01-31 10:54:23 -0500

    M6e DONE M5 DONE

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -13,43 +13,52 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # -------------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DONE: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # -------------------------------------------------------------------------
     window = tkinter.Tk()
     # window.mainloop()
     # -------------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DONE: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # -------------------------------------------------------------------------
     frame1 = ttk.Frame(window, padding=30)
     frame1.grid()
     # window.mainloop()
     # -------------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DONE: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # -------------------------------------------------------------------------
-    button = ttk.Button(frame1)
-    button.grid()
+    button1 = ttk.Button(frame1, text="PUT WORDS IN ME")
+    # button1.grid()
     # window.mainloop()
     # -------------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # DONE: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
-    button['command'] =  print("Hello")
-    window
+    # button1['command'] = (lambda: print('Hello'))
+    # window.mainloop()
     # -------------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # -------------------------------------------------------------------------
+    my_entry_box = ttk.Entry(frame1)
+    my_entry_box.grid()
+
+    # button1['command'] = (lambda: todo_six(my_entry_box))
+    button1.grid()
+
+    button2 = ttk.Button(frame1, text="Button 2")
+    button2.grid()
+    # window.mainloop()
 
     # -------------------------------------------------------------------------
-    # TODO: 7.
+    # DONE: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -71,7 +80,14 @@ def main():
     #      s = entry_box.get()
     #      n = int(s)
     ####################################################################
+    my_entry_box2 = ttk.Entry(frame1)
+    my_entry_box2.grid()
 
+    button3 = ttk.Button(frame1, text="Button 3")
+    button3['command'] = (lambda: todo_seven(my_entry_box, my_entry_box2))
+    button3.grid()
+
+    window.mainloop()
     # -------------------------------------------------------------------------
     # TODO: 8. As time permits, do other interesting GUI things!
     # -------------------------------------------------------------------------
@@ -80,4 +96,22 @@ def main():
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
+
+
+def todo_six(entry_box):
+    contents = entry_box.get()
+    if contents == 'ok':
+        print("Hello")
+    else:
+        print("Goodbye")
+
+
+def todo_seven(entry_box, entry_box2):
+    s = entry_box.get()
+    n = int(entry_box2.get())
+
+    for k in range(n):
+        print(s)
+
+
 main()
